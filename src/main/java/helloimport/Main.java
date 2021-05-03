cb9f60310e6da8f63adb8d77d7479a0d46e79212
david.van.steertegem@gmail.com <PowerUser420>
2021-05-03 13:56:53 +0200

    Initial commit

---
+++ src/main/java/helloimport/Main.java
@@ -0,0 +1,9 @@
+package helloimport;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, String> pair = Pair.of("left", "right");
+    }
+}
